e7571e231d24b389305e100eb04148cfeee4b536
richardxnathan <richardnathand@gmail.com>
2024-01-10 17:03:31 -0300

    Aula 02

--- ScreenSound/ScreenSound/Program.cs
+++ ScreenSound/ScreenSound/Program.cs
@@ -1,9 +1,15 @@
-﻿using (HttpClient client = new HttpClient())
+﻿using ScreenSound.Modelos;
+using System.Text.Json;
+
+using (HttpClient client = new HttpClient())
 {
+
     try
     {
         string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        musicas[1999].ExibirDetalhesDaMusica();
+
     }
     catch (Exception ex)
     {
